ade5ec97e01716aa2426c88712a9bf48ddb8d9c9
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-09 15:48:38 -0400

    Agregar proyecto

--- listar/columnas.php
+++ listar/columnas.php
@@ -7,30 +7,23 @@ class columnas {
     public $a;
 
     public function __construct($listas) {
-        $clients = (new $listas())->getList();
-        //var_dump($clients[1]->getFields());
-        //$algo = $clients[1];
-//        var_dump($algo->getFields());
-//        var_dump($algo->getMeta());
-        //var_dump($clients->getMetaIdx());
-        //$arreglo = (array) $clients->_fields;
-        //var_dump($arreglo);
-        foreach ($clients as $client) {
-            //var_dump($client->getFields());
-            //var_dump($client->getMeta());
-            if ($client->getMeta()) {
-                foreach ($client->getMeta() as $key => $value) {
+        $listados = (new $listas())->getList();
+       
+        foreach ($listados as $listado) {
+            
+            if ($listado->getMeta()) {
+                foreach ($listado->getMeta() as $key => $value) {
                     $campos[$key] = $value;
                 }
-                //var_dump($campos);
+                
             }
-            if ($client->getFields()){
-                foreach ($client->getFields() as $value) {
-                    $campos[$value] = $client->get($value);  
+            if ($listado->getFields()){
+                foreach ($listado->getFields() as $value) {
+                    $campos[$value] = $listado->get($value);  
                 }
             }
-            //var_dump($campos);
-            $clientList[]=$campos;
+            
+            $listaFinal[]=$campos;
             unset($campos);
             //var_dump($campos);
             
@@ -41,7 +34,9 @@ class columnas {
 
             //echo "$client ";
         }
-        var_dump($clientList);
+        
+        return $listaFinal;
+        //var_dump($clientList);
     }
 
 }
