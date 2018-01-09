4c5606f50d57a63e4a05eb85e034272dad7647e3
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-09 19:04:13 -0400

    Actualizacion de client

--- client/list.php
+++ client/list.php
@@ -3,19 +3,11 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 include_once '../config/config.php';
 
-$clientsList=array();
-$users = (new GT_Client_List())->getList();
-
-foreach($users as $user)
-{
-    $clientsList[]=array
-    (
-        'ID'=>$user->get('ID'),
-        'name'=>$user->get('name')
-    );
-}
-
+$c= new Lists();
+$b = $c->listing('GT_Client_List');
+//var_dump($b);
 $response['status']='success';
 $response['msg']='Complete';
-$response['data']=$clientsList;
+$response['data'] = $b;
+
 die;
