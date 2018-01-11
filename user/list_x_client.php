83dfc15bac6e7be7b20213d6d548153a44918595
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-11 15:40:53 -0400

    Subir listXclient

--- user/list_x_client.php
+++ user/list_x_client.php
@@ -9,22 +9,19 @@ class List_X_Client {
     public function listxclient($appClientID) {
         $crmDB = new usersSql();
         $crmDBconn = $crmDB->connect(_TOURTRACK_USERS_DATABASE, _TOURTRACK_USERS, _TOURTRACK_USERS_PASSWORD, 'users');
-        $query = $crmDB->query('SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from x_apps_clients where ID='.$appClientID.')');
-        //var_dump();
+        $query = $crmDB->query('SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from x_apps_clients where ID=' . $appClientID . ')');
+        var_dump($query);
         while ($row = $crmDB->fetch_array($query)) {
             $user = new GT_User($row['userID']);
             if ($user->getMeta()) {
                 $campos['ID'] = $user->get('ID');
                 foreach ($user->getMeta() as $key => $value) {
-                  
-                    $campos[$key] = $value;
-                   
-                }
-                $finalList[] = $campos;
-                unset($campos);
-            }
 
-            
+                    $campos[$key] = $value;
+                }
+            }
+            $finalList[] = $campos;
+            unset($campos);
         }
         return $finalList;
     }
@@ -35,7 +32,7 @@ class List_X_Client {
 $a = new List_X_Client();
 $b = $a->listxclient($_REQUEST['appClientID']);
 //var_dump($b);
-$response['status']='success';
-$response['msg']='Complete';
+$response['status'] = 'success';
+$response['msg'] = 'Complete';
 $response['data'] = $b;
 die;
