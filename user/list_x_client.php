193604ac2be2cce286cfcd7ec3866cf3cfcc5104
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-11 15:22:19 -0400

    Subir listXclient

---
+++ user/list_x_client.php
@@ -0,0 +1,39 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+include_once '../config/config.php';
+
+class List_X_Client {
+
+    public function listxclient() {
+        $crmDB = new usersSql();
+        $crmDBconn = $crmDB->connect(_TOURTRACK_USERS_DATABASE, _TOURTRACK_USERS, _TOURTRACK_USERS_PASSWORD, 'users');
+        $query = $crmDB->query('SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from x_apps_clients where ID=12)');
+        while ($row = $crmDB->fetch_array($query)) {
+            $user = new GT_User($row['userID']);
+            if ($user->getMeta()) {
+                $campos['ID'] = $user->get('ID');
+                foreach ($user->getMeta() as $key => $value) {
+
+                    $campos[$key] = $value;
+                }
+                $finalList[] = $campos;
+                unset($campos);
+            }
+
+            
+        }
+        return $finalList;
+    }
+
+    //SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from x_apps_clients where ID=12);
+}
+
+$a = new List_X_Client();
+$b = $a->listxclient();
+//var_dump($b);
+$response['status']='success';
+$response['msg']='Complete';
+$response['data'] = $b;
+die;
