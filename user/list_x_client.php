fcfeed2d92fe12e52c8ad9e7f11f75fc2d88dacd
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-11 15:35:24 -0400

    Subir listXclient

--- user/list_x_client.php
+++ user/list_x_client.php
@@ -9,14 +9,16 @@ class List_X_Client {
     public function listxclient($appClientID) {
         $crmDB = new usersSql();
         $crmDBconn = $crmDB->connect(_TOURTRACK_USERS_DATABASE, _TOURTRACK_USERS, _TOURTRACK_USERS_PASSWORD, 'users');
-        $query = $crmDB->query('SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from x_apps_clients where ID=$appClientID)');
+        $query = $crmDB->query('SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from x_apps_clients where ID='.$appClientID.')');
+        //var_dump();
         while ($row = $crmDB->fetch_array($query)) {
             $user = new GT_User($row['userID']);
             if ($user->getMeta()) {
                 $campos['ID'] = $user->get('ID');
                 foreach ($user->getMeta() as $key => $value) {
-
+                  
                     $campos[$key] = $value;
+                   
                 }
                 $finalList[] = $campos;
                 unset($campos);
