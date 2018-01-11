d848426abda90f5369b18be97a68b754626b760b
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-11 14:16:24 -0400

    Subir appclient

---
+++ appclient/list.php
@@ -0,0 +1,13 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+include_once '../config/config.php';
+
+$c= new Lists();
+$b = $c->listing('GT_X_App_Client_List');
+//var_dump($b);
+$response['status']='success';
+$response['msg']='Complete';
+$response['data'] = $b;
+
+die;
