04484b678f5e744c8fe93e8515439c760d907fa2
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-11 16:15:28 -0400

    Subir listXclient

--- user/list_x_client.php
+++ user/list_x_client.php
@@ -7,17 +7,23 @@ include_once '../config/config.php';
 class List_X_Client {
 
     public function listxclient($appClientID) {
-        $crmDB = new usersSql();
-        $crmDBconn = $crmDB->connect(_TOURTRACK_USERS_DATABASE, _TOURTRACK_USERS, _TOURTRACK_USERS_PASSWORD, 'users');
-        $query = $crmDB->query('SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from x_apps_clients where ID=' . $appClientID . ')');
-        var_dump($query);
-        while ($row = $crmDB->fetch_array($query)) {
+        $finalList = array();
+        $usersDB = new usersSql();
+        $usersDBconn = $usersDB->connect(_AURORA_USERS_DATABASE, _AURORA_USERS, _AURORA_USERS_PASSWORD, 'users');
+        //var_dump($usersDBconn);
+        $query = $usersDB->query('SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from `x_apps_clients` where ID="' . $appClientID . '")');
+        //var_dump($usersDB->fetch_array($query));
+        while ($row = $usersDB->fetch_array($query)) {
             $user = new GT_User($row['userID']);
             if ($user->getMeta()) {
                 $campos['ID'] = $user->get('ID');
                 foreach ($user->getMeta() as $key => $value) {
-
-                    $campos[$key] = $value;
+                    if ($key == "first_name") {
+                        $campos[$key] = $value;
+                    }
+                    if ($key == "last_name") {
+                        $campos[$key] = $value;
+                    }
                 }
             }
             $finalList[] = $campos;
@@ -29,6 +35,7 @@ class List_X_Client {
     //SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from x_apps_clients where ID=12);
 }
 
+//var_dump($_REQUEST['appClientID']);
 $a = new List_X_Client();
 $b = $a->listxclient($_REQUEST['appClientID']);
 //var_dump($b);
