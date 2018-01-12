983b29c57c17a47e0ce731ff06199ac6b13040f8
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-12 18:58:46 -0400

    Actualización

--- objects/GetOne.class.php
+++ objects/GetOne.class.php
@@ -1,9 +1,9 @@
 <?php
 class GetOne
 {
-    public function getOne($class, $ID, $field)
+    public function get_one($class, $ID, $field='ID')
     {
-        $listing = (new $class($ID));
+        $listing = (new $class($ID, $field));
         if ($listing->getFields()) 
             foreach ($listing->getFields() as $value) 
                 $campos[$value] = $listing->get($value);
