b74214319daf3c117a39c0db87145ce724b2e717
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-12 18:12:54 -0400

    Subiendo xusersrol

---
+++ role/x_users_rol.php
@@ -0,0 +1,39 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+include_once '../config/config.php';
+
+class X_Users_Rol {
+
+    public function xusersrol($appClientID) {
+        $usersDB = new usersSql();
+        $usersDBconn = $usersDB->connect(_AURORA_USERS_DATABASE, _AURORA_USERS, _AURORA_USERS_PASSWORD, 'users');
+        //var_dump($usersDBconn);
+        $query = $usersDB->query("SELECT * FROM `x_users_roles` WHERE appClientID=$appClientID GROUP BY userID");
+        while ($row = $usersDB->fetch_array($query)) {
+            $id = $row["userID"];
+            $user = new GT_User($row["userID"]);
+            if ($user->getMeta()) {
+                $campos['ID'] = $user->get('ID');
+                //  var_dump($campos);
+                foreach ($user->getMeta() as $key => $value) {
+                    
+                        $campos[$key] = $value;
+
+                }
+            }
+            $finalList[] = $campos;
+            unset($campos);
+        }
+        return $finalList;
+    }
+
+}
+
+$a = new X_Users_Rol();
+$b = $a->xusersrol($_REQUEST['appClientID']);
+$response['status'] = 'success';
+$response['msg'] = 'Complete';
+$response['data'] = $b;
+die;
