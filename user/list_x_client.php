af8dbf1653fdb79536dac00915ef658a4b87809f
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-11 20:19:08 -0400

    Lista por app

--- user/list_x_client.php
+++ user/list_x_client.php
@@ -11,7 +11,10 @@ class List_X_Client {
         $usersDB = new usersSql();
         $usersDBconn = $usersDB->connect(_AURORA_USERS_DATABASE, _AURORA_USERS, _AURORA_USERS_PASSWORD, 'users');
         //var_dump($usersDBconn);
-        $query = $usersDB->query('SELECT userID FROM `x_users_clients` WHERE clientID in (select clientID from `x_apps_clients` where ID="' . $appClientID . '")');
+        $query = $usersDB->query('SELECT users_master.ID FROM users_master 
+JOIN x_users_clients ON users_master.ID = x_users_clients.userID
+JOIN  x_apps_clients ON  x_users_clients.clientID = x_apps_clients.clientID
+join users_password on x_apps_clients.ID = users_password.appClientID and users_password.userID=users_master.ID and users_password.appClientID='.$appClientID.'');
         //var_dump($usersDB->fetch_array($query));
         while ($row = $usersDB->fetch_array($query)) {
             $user = new GT_User($row['userID']);
