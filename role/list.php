5418105ee339353a0afacb13f7ac1db0eb2db040
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-15 12:18:17 -0400

    Actualización

--- role/list.php
+++ role/list.php
@@ -4,7 +4,14 @@ header("Content-Type: application/json; charset=UTF-8");
 include_once '../config/config.php';
 
 $c= new Lists();
-$b = $c->listing('GT_Role_List', array($_REQUEST['appClientID'], 'appClientID'));
+if (isset($_REQUEST['appClientID'])) {
+    $b = $c->listing('GT_Role_List', array($_REQUEST['appClientID'], 'appClientID'));
+} elseif (isset($_REQUEST['rolID'])) {
+    $b = $c->listing('GT_Role_List', array($_REQUEST['rolID'], 'ID'));
+}else{
+    $b = $c->listing('GT_Role_List');
+}
+
 //var_dump($b);
 $response['status']='success';
 $response['msg']='Complete';
