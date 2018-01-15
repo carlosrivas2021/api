f05b294ba14e7aaff9f47982d15597dea3c51e34
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-15 17:38:43 -0400

    Actualización

--- permission/permission_x_rol.php
+++ permission/permission_x_rol.php
@@ -6,15 +6,21 @@ include_once '../config/config.php';
 
 class Permission_X_Rol {
 
-    public function permissionxrol($appClientID) {
+    public function permissionxrol($appClientID, $rolID = '') {
         $finalList = array();
         $usersDB = new usersSql();
         $usersDBconn = $usersDB->connect(_AURORA_USERS_DATABASE, _AURORA_USERS, _AURORA_USERS_PASSWORD, 'users');
         //var_dump($usersDBconn);
-        $query = $usersDB->query('select * from permissions where permissions.app in ( select x_apps_clients.appID from x_apps_clients where x_apps_clients.ID="'.$appClientID.'")');
-       // var_dump($usersDB->fetch_array($query));
+        if ($rolID == '') {
+
+            $query = $usersDB->query('select * from permissions where permissions.app in ( select x_apps_clients.appID from x_apps_clients where x_apps_clients.ID="' . $appClientID . '")');
+        } else {
+            $query = $usersDB->query('SELECT permissionID FROM `x_roles_permissions` where roleID' . $rolID . '');
+        }
+
+        // var_dump($usersDB->fetch_array($query));
         while ($row = $usersDB->fetch_array($query)) {
-                               
+
             $finalList[] = $row;
             //unset($campos);
         }
@@ -26,7 +32,12 @@ class Permission_X_Rol {
 
 //var_dump($_REQUEST['appClientID']);
 $a = new Permission_X_Rol();
-$b = $a->permissionxrol($_REQUEST['appClientID']);
+if (isset($_REQUEST['roleID'])) {
+    $b = $a->permissionxrol($_REQUEST['roleID']);
+} else {
+    $b = $a->permissionxrol($_REQUEST['appClientID']);
+}
+
 //var_dump($b);
 $response['status'] = 'success';
 $response['msg'] = 'Complete';
