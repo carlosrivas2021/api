7cb61e0dbc11c7985d072fbfeac769d0a05feef7
carl0s0x <ingcarlosrivas20@gmail.com>
2018-01-16 18:23:37 -0400

    Actualización

--- permission/listtree.php
+++ permission/listtree.php
@@ -1,9 +1,9 @@
 <?php
 
-//header("Access-Control-Allow-Origin: *");
-//header("Content-Type: application/json; charset=UTF-8");
-//include_once '../config/config.php';
-require_once '../objects/sql.class.php';
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+include_once '../config/config.php';
+//require_once '../objects/sql.class.php';
 require_once './addTree.php';
 
 class List_Tree {
@@ -13,7 +13,7 @@ class List_Tree {
 
     public function get() {
         $usersDB = new usersSql();
-        $usersDBconn = $usersDB->connect('localhost', 'root', '200306', 'users');
+        $usersDBconn = $usersDB->connect(_AURORA_USERS_DATABASE, _AURORA_USERS, _AURORA_USERS_PASSWORD, 'users');
 
         $query = $usersDB->query('SELECT * FROM permissions');
         $this->_elements["masters"] = $this->_elements["childrens"] = array();
@@ -56,21 +56,26 @@ class List_Tree {
 $tree = new List_Tree();
 
 $elements = $tree->get();
-$masters = $elements["masters"];
+$b['masters'] = $elements["masters"];
 //var_dump($masters);
-$childrens = $elements["childrens"];
+$b['children'] = $elements["childrens"];
 //var_dump($childrens);
-echo "<ul>";
-foreach ($masters as $master) {
 
-    echo '<li style="margin: 5px 0px">';
-    echo "<span><i class='glyphicon glyphicon-folder-open'></i></span>";
-    echo '<a href="#" data-status="' . $master["have_childrens"];
-    echo '       style="margin: 5px 6px" class="btn btn-warning btn-xs btn-folder">';
-
-    echo $master["name"] . '</a>';
-    echo Add_Tree::nested($childrens, $master["ID"]);
-    echo "</li>";
-}
-
-echo "</ul>";
+$response['status'] = 'success';
+$response['msg'] = 'Complete';
+$response['data'] = $b;
+die;
+//echo "<ul>";
+//foreach ($masters as $master) {
+//
+//    echo '<li style="margin: 5px 0px">';
+//    echo "<span><i class='glyphicon glyphicon-folder-open'></i></span>";
+//    
+//    echo '       style="margin: 5px 6px" class="btn btn-warning btn-xs btn-folder">';
+//
+//    echo $master["name"] . '</a>';
+//    echo Add_Tree::nested($childrens, $master["ID"]);
+//    echo "</li>";
+//}
+//
+//echo "</ul>";
